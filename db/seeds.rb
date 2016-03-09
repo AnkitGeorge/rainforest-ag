d7f4bcba934bcd00e060a5e8c32d77648815b199
AnkitGeorge <ankit.george@gmail.com>
2016-03-08 23:03:06 -0500

    Generate the products controller and views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(
+  :name  => "Snapchat follower drone",
+  :description => "This drone follows you everywhere and uploads moments it finds appropriate to Snapchat.",
+  :price_in_cents    => 13500
+)
+
+Product.create!(
+  :name  => "Old map of Toronto",
+  :description => "One of the oldest maps of the city of Toronto, then called York.",
+  :price_in_cents    => 8800
+)
